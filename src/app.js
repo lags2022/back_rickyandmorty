ce9e3e7fdfe34912d58e3390c9b0d8723d519205
lags2022 <lguzman.58erb@outlook.com>
2023-02-22 17:09:51 -0500

    update 2

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use("/rickandmorty", getCharacters);
 
 app.get("/", (req, res) => {
-  res.send("bienvenido");
+  res.send("bienvenido a esta aplicacion");
 });
 
 module.exports = app;
