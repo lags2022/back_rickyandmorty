54bf17868e1d55d2dff04e5b5b66a9e6c2c210d1
lags2022 <lguzman.58erb@outlook.com>
2023-08-07 20:55:27 -0500

    update

--- src/db/index_db.js
+++ src/db/index_db.js
@@ -1,25 +1,23 @@
 const { Sequelize } = require("sequelize");
 const favFunc = require("./models/Favorite");
 const userFunc = require("./models/User");
+require("dotenv").config();
 
-// vuelve a descomentar esto cuando estes en local , vuelvo a comentarlo para que funcione en flyio
-// require("dotenv").config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DB } = process.env;
 
-// const { DB_USER, DB_PASSWORD, DB_HOST, DB_DB } = process.env;
+const database = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
+  {
+    logging: false,
+  }
+);
 
-// const database = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
-//   { logging: false }
-// );
+// const DATABASE_URL = process.env.DATABASE_URL;
 
-const DATABASE_URL = process.env.DATABASE_URL;
-
-const database = new Sequelize(DATABASE_URL)
+// const database = new Sequelize(DATABASE_URL)
 
 favFunc(database);
 userFunc(database);
-
-console.log(process.env.SECRET)
 
 //relacion de usuarios con favoritos es de n a n
 const { User, Favorite } = database.models;
